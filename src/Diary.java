f281d49508ce3966baf80fb703be65de7939a0e7
hShamed <h.shamed.ar@gmail.com>
2023-10-04 10:51:05 -0600

    Creación de la ventana de 'Inicio de Sesión'

---
+++ src/Diary.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Diary {
+    private JPanel panel1;
+    private JTextField userTxtF;
+    private JPasswordField passwordTxtF;
+    private JButton loginBtn;
+    private JButton registerBtn;
+    private JLabel passwordLabel;
+    private JLabel userLabel;
+    private JLabel loginLabel;
+}
